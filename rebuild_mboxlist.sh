398bb602bd596bba73dad781ffec1a1528af21d1
Daniel Schmidt <dschmidt02@gmail.com>
2018-04-05 11:38:17 +0200

    initial commit script

---
+++ rebuild_mboxlist.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+
+echo -e "stopping cyrus..\n"
+systemctl stop cyrus-imapd
+
+echo -e "backup mailboxes.db..\n"
+mv /var/lib/imap/mailboxes.db /var/lib/imap/mailboxes.db.$$
+
+echo -e "building new mailboxes.db flat file..\n"
+find /var/spool/imap/?/user -maxdepth 1 -mindepth 1 | \
+  while read i; do
+    k=$(basename $i)
+    echo -e "user.$k\tdefault\t$k\tlrswipcda\timap_user\tlrswipcda" >> /tmp/mboxflat.txt
+  done
+
+echo -e "importing mailboxes flat file in new mailboxes.db..\n"
+/usr/lib/cyrus-imapd/ctl_mboxlist -u </tmp/newmboxlist.txt
+rm -f /tmp/mboxflat.txt
+echo -e "starting reconstruct\n"
+
+find /var/spool/imap/?/user -maxdepth 1 -mindepth 1 | \
+  while read i; do
+        /usr/lib/cyrus-imapd/reconstruct -rf user.$(basename $i)
+  done
+
+echo -e "restarting cyrus..\n"
+systemctl start cyrus-imapd
+
